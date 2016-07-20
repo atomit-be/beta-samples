9b72557904b86e5da7671c997e33be34fd6bf092
cambierr <romain@shareif.com>
2016-07-20 16:21:48 +0200

    added java sample

---
+++ java-rabbitmq/src/main/java/be/atomit/beta/samples/java/rabbitmq/SendToDevice.java
@@ -0,0 +1,90 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2016 cambierr.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package be.atomit.beta.samples.java.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ *
+ * @author cambierr
+ */
+public class SendToDevice {
+
+    public static void main(String[] args) throws Exception {
+        /**
+         * Your appEUI as found in your atomit dashboard
+         */
+        String appEUI = "70B3D57ED000054E";
+        /**
+         * Your devEUI as found in your atomit dashboard
+         */
+        String devEUI = "0000000033152A74";
+        /**
+         * The username as defined in your atomit dashboard
+         */
+        String username = "demo";
+        /**
+         * The password as defined in your atomit dashboard
+         */
+        String password = "demo";
+        
+
+        ConnectionFactory factory = new ConnectionFactory();
+        /**
+         * The atomit rabbitmq cluster address
+         */
+        factory.setHost("api.beta.atomit.be");
+        /**
+         * For now, the VHost to use will always be "beta"
+         */
+        factory.setVirtualHost("beta");
+        /**
+         * The real username is appEUI_username:
+         */
+        factory.setUsername(appEUI + "_" + username);
+        factory.setPassword(password);
+
+        Connection connection = factory.newConnection();
+        System.out.println("Connected to atomit rabbitMQ cluster !");
+        
+        Channel channel = connection.createChannel();
+
+        String message
+                = "{"
+                + "\"payload\": \"c2FsdXQ=\","
+                + "\"port\": 1,"
+                + "\"ttl\": \"1h\""
+                + "}";
+        
+        /**
+         * Send our message
+         */
+        channel.basicPublish(appEUI, devEUI + "." + "down", null, message.getBytes());
+        
+        System.out.println("Message sent!");
+    }
+    
+}
